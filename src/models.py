fa8d2f74d8266108b76ae09fc8e43674bef1279c
Me-Phew <basmateusz@wp.pl>
2022-01-02 00:46:49 +0100

    Create simple tables models

--- src/models.py
+++ src/models.py
@@ -9,7 +9,10 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
     password = Column(String, nullable=False)
+    sex = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
 
@@ -17,13 +20,21 @@ class User(Base):
 class Service(Base):
     __tablename__ = "services"
     id = Column(Integer, primary_key=True, nullable=False)
-    user_defined = Column(Boolean, default=False, nullable=False)
+    name = Column(String, nullable=False)
+    user_defined = Column(Boolean, nullable=False)
+    created_by = Column(Integer, ForeignKey('users.id'), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
 
 
 class Appointment(Base):
     __tablename__ = "appointments"
     id = Column(Integer, primary_key=True, nullable=False)
-    user_id = relationship("User")
+    service_id = Column(Integer, ForeignKey('services.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    scheduled_for = Column(TIMESTAMP(timezone=True), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
 
 
 class Setting(Base):
