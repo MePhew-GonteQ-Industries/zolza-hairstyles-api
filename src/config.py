6faf474a02d5f4122caa10262dec3eb79a460417
Me-Phew <basmateusz@wp.pl>
2022-01-03 21:04:07 +0100

    Add mail server configuration and API version and title strings

--- src/config.py
+++ src/config.py
@@ -1,16 +1,31 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
+    API_VERSION: str
+    API_TITLE: str
     BASE_URL: str
+
     DATABASE_USERNAME: str
     DATABASE_PASSWORD: str
     DATABASE_HOSTNAME: str
     DATABASE_PORT: str
     DATABASE_NAME: str
+
     API_SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: EmailStr
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_TLS: bool
+    MAIL_SSL: bool
+    USE_CREDENTIALS: bool
+    VALIDATE_CERTS: bool
+    MAIL_FROM_NAME: str
 
     class Config:
         env_file = ".env"
