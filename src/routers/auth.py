7208758b63da2c7ba8fd457e3ee5e5c82f9a7928
Me-Phew <basmateusz@wp.pl>
2022-01-03 21:07:26 +0100

    Add endpoint for logging in

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from sqlalchemy.orm import Session
 from ..config import settings
+from ..database import get_db
+from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix=settings.BASE_URL + '/auth',
                    tags=['Authorization'])
+
+
+@router.post('/login')
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    return user_credentials
 
 
 @router.get('/')
