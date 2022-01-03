d461bed57bb1929ab7bf53bca761d9f60d808bc3
Me-Phew <basmateusz@wp.pl>
2022-01-03 21:10:01 +0100

    Add enums and pydantic models for user settings

--- src/schemas/user_settings.py
+++ src/schemas/user_settings.py
@@ -1,9 +1,34 @@
 from pydantic import BaseModel
+from enum import Enum
+from typing import Union
 
 
-class BaseUserSettings(BaseModel):
-    language: str
-    prefered_theme: str
+class AvailableContentLanugages(str, Enum):
+    polish = "pl"
+    english = "en"
+
+
+class AvailableThemes(str, Enum):
+    dark = 'dark'
+    light = 'light'
+
+
+class PreferedTheme(BaseModel):
+    name: str = 'prefered_theme'
+    default_value: AvailableThemes = None
+    current_value: AvailableThemes
+
+
+class Language(BaseModel):
+    name: str = 'language'
+    default_value: AvailableContentLanugages = None
+    current_value: AvailableContentLanugages
+
+
+class ReturnSettings(BaseModel):
+    name: str
+    default_value: Union[str, None]
+    current_value: str
 
     class Config:
         orm_mode = True
