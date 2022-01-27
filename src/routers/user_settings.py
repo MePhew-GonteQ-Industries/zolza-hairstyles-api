03a551fec44b2a1c82e9fdbe5a5420290b64a42a
Me-Phew <basmateusz@wp.pl>
2022-01-27 12:56:12 +0100

    Add scaffold version of this file because of dependency requirements

--- src/routers/user_settings.py
+++ src/routers/user_settings.py
@@ -1,17 +1,37 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from .. import models, oauth2
 from ..config import settings
-from ..schemas.user_settings import PreferedTheme, Language
+from ..database import get_db
+from ..schemas.user_settings import ReturnSettings, UpdateSettings
 
 router = APIRouter(prefix=settings.BASE_URL + '/settings',
                    tags=['Settings'])
 
 
-@router.get('')
-async def get_settings():
-    return {'language': 'en-GB',
-            'prefered_theme': 'dark'}
+@router.get('', response_model=ReturnSettings)
+async def get_settings(db: Session = Depends(get_db),
+                       user=Depends(oauth2.get_user)):
+    user_settings = db.query(models.Setting).where(models.Setting.user_id == user.id).all()
+
+    return {'settings': user_settings}
 
 
-@router.post('', status_code=status.HTTP_201_CREATED)
-async def update_settings():
-    return {'test': 'normal :D'}
+@router.put('')  # , response_model=ReturnSettings
+async def update_settings(new_settings: UpdateSettings,
+                          db: Session = Depends(get_db),
+                          user=Depends(oauth2.get_user)):
+
+    settings_db = []
+
+    for setting in new_settings.settings:
+        setting_db = db.query(models.Setting).where(models.Setting.user_id == user.id
+                                                    and models.Setting.name == setting.get('name')).first()
+        setting_db.current_value = setting.get('current_value')
+
+        db.commit()
+        db.refresh(setting_db)
+        settings_db.append(setting_db)
+
+    return {'settings': settings_db}
