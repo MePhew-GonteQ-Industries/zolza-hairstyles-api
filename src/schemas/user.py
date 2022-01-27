9aa040aa5649a929befd72fe61cdfbdeca3aadf1
Me-Phew <basmateusz@wp.pl>
2022-01-27 12:49:56 +0100

    Enforce strong passwords

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,7 +1,8 @@
+import re
 from datetime import datetime
 from enum import Enum
-from pydantic import BaseModel, EmailStr
-from .user_settings import ReturnSettings
+from pydantic import BaseModel, EmailStr, Field, validator
+from .user_settings import ReturnSetting
 from typing import List
 
 
@@ -11,29 +12,41 @@ class Gender(str, Enum):
     other = 'other'
 
 
-class BaseUser(BaseModel):
+class UserEmailOnly(BaseModel):
     email: EmailStr
-    name: str
-    surname: str
-    gender: Gender
 
     class Config:
         orm_mode = True
 
 
+class BaseUser(UserEmailOnly):
+    name: str = Field(max_length=50)
+    surname: str = Field(max_length=50)
+    gender: Gender
+
+
 class CreateUser(BaseUser):
-    password: str
+    password: str = Field(min_length=8, max_length=200)
+
+    @validator('password')
+    def verify_strong_password(cls, v):
+        strong_password_regex = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,}$"
+
+        if re.fullmatch(strong_password_regex, v):
+            return v
+        else:
+            raise ValueError('ensure the password is strong')
 
 
 class ReturnUser(BaseUser):
-    permission_level: list[str]
+    permission_level: List[str]
     verified: bool
     created_at: datetime
 
 
 class ReturnUserAndSettings(BaseModel):
     user: ReturnUser
-    settings: List[ReturnSettings]
+    settings: List[ReturnSetting]
 
     class Config:
         orm_mode = True
