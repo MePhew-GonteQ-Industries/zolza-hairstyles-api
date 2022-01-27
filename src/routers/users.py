148e61e523c047d71a058c95a25fa553b429755f
Me-Phew <basmateusz@wp.pl>
2022-01-27 12:48:49 +0100

    Implement email verification endpoints

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,14 +1,21 @@
+from datetime import datetime, timedelta
 from fastapi import APIRouter, status, Depends, BackgroundTasks, HTTPException, Header
 from ..config import settings
 from sqlalchemy.orm import Session
 from ..database import get_db
-from ..schemas.user import CreateUser, ReturnUser, ReturnUserAndSettings
-from ..schemas.user_settings import AvailableContentLanugages, AvailableThemes, PreferedTheme, Language
+from ..exceptions import CooldownHTTPException, InvalidTokenException, MalformedAccessTokenException, \
+    InvalidEnumerationMemberHTTPException
+from ..schemas.oauth2 import TokenType
+from ..schemas.user import CreateUser, ReturnUser, ReturnUserAndSettings, UserEmailOnly
+from ..schemas.user_settings import AvailableContentLanugages, AvailableSettings, AvailableThemes, PreferredThemeBase, \
+    LanguageCreate
+from ..schemas.email_request import EmailRequestType, EmailVerificationRequest
 from .. import models
-from fastapi_mail import MessageSchema
-from ..email import send_email
+from ..email_manager import create_email_verification_email, create_email_request, send_email
 from sqlalchemy.exc import IntegrityError
 from pydantic import Required
+from .. import utils, oauth2
+from ..utils import on_decode_error
 
 router = APIRouter(prefix=settings.BASE_URL + '/users',
                    tags=['Users'])
@@ -19,13 +26,13 @@ def create_user(user: CreateUser,
                 background_tasks: BackgroundTasks,
                 db: Session = Depends(get_db),
                 content_language: AvailableContentLanugages = Header(Required),
-                prefered_theme: AvailableThemes = Header(Required)):
+                preferred_theme: AvailableThemes = Header(Required)):
     """
     ## Part of the user creation process is initializing the user's settings
-    ### For this reason it is required to pass two *header parameters*: `content-language` and `prefered-theme` to this endpoint
+    ### For this reason it is required to pass two *header parameters*: `content-language` and `preferred-theme` to this endpoint
     ### Later on these settings can be accessed and modified via *`/api/settings/`* endpoint
 
-    #### `content-language` is a **required** *header parameter* used to represent user's prefered content language.
+    #### `content-language` is a **required** *header parameter* used to represent user's preferred content language.
 
     It can only be set to one of the following two values as these are the only languages currently supported
     both by this API and [Zołza Hairstyles](https://mephew.ddns.net) website :
@@ -41,62 +48,31 @@ def create_user(user: CreateUser,
     permissions updates or upcoming appointments notices)
 
 
-    #### `prefered-theme` is a **required** *header parameter* used to represent user's prefered application theme
+    #### `preferred-theme` is a **required** *header parameter* used to represent user's preferred application theme
 
     It can only be set to one of the following two values as these are the only themes currently supported
     by [Zołza Hairstyles](https://mephew.ddns.net) website :
     - **dark** for dark theme
     - **light** for light theme
 
-    This value will be saved in the database as the user's prefered theme in the created user's settings.
+    This value will be saved in the database as the user's preferred theme in the created user's settings.
 
     This particular endpoint uses this value only for the sole purpose of user config initialization in order to
     provide consistent theming experience across multiple devices and/or web browsers
     """
 
-    user_theme = PreferedTheme(current_value=prefered_theme)
+    user_theme = PreferredThemeBase(current_value=preferred_theme)
 
     if content_language == content_language.polish:
-        template_name = 'account_verification_pl.html'
-        subject = 'Zołza Hairstyles - weryfikacja konta'
-        user_language = Language(current_value=AvailableContentLanugages.polish)
+        user_language = LanguageCreate(current_value=AvailableContentLanugages.polish)
     elif content_language == content_language.english:
-        template_name = 'account_verification_en.html'
-        subject = 'Zołza Hairstyles - account verification'
-        user_language = Language(current_value=AvailableContentLanugages.english)
+        user_language = LanguageCreate(current_value=AvailableContentLanugages.english)
     else:
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                            detail=[
-                                {
-                                    "loc": [
-                                        "header",
-                                        "content-language"
-                                    ],
-                                    "msg": "value is not a valid enumeration member; permitted: 'pl', 'en'",
-                                    "type": "type_error.enum",
-                                    "ctx": {
-                                        "enum_values": [
-                                            "pl",
-                                            "en"
-                                        ]
-                                    }
-                                }
-                            ])
-
-    message = MessageSchema(
-        subject=subject,
-        recipients=[user.email],
-        template_body={
-            'user': user.name,
-            'account_confirmation_link': 'https://mephew.ddns.net/account/activate/213414'
-        },
-        subtype="html"
-    )
-
-    background_tasks.add_task(send_email, message, template_name)
+        raise InvalidEnumerationMemberHTTPException
 
     new_user = user.dict()
-    new_user['hashed_password'] = new_user.pop('password')
+
+    new_user['hashed_password'] = utils.hash_password(new_user.pop('password'))
 
     new_user = models.User(**new_user)
     db.add(new_user)
@@ -105,7 +81,7 @@ def create_user(user: CreateUser,
         db.commit()
     except IntegrityError:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail='duplicate key value violates unique constraint user_email')
+                            detail=f'user with an email address of {user.email} already exists')
 
     db.refresh(new_user)
 
@@ -123,12 +99,129 @@ def create_user(user: CreateUser,
 
     user_settings = db.query(models.Setting).filter(models.Setting.user_id == new_user.id).all()
 
+    email_verification_request = create_email_request(user=user,
+                                                      token_type=TokenType.email_verification_token,
+                                                      request_type=EmailRequestType.email_verification_request)
+
+    db.add(email_verification_request)
+
+    message, template_name = create_email_verification_email(content_language,
+                                                             new_user,
+                                                             email_verification_request.request_token)
+
+    background_tasks.add_task(send_email, message, template_name)
+
     return {'user': new_user,
             'settings': user_settings}
 
 
-@router.get('/me', response_model=ReturnUser)
-async def me(db: Session = Depends(get_db)):
-    # user = db.query(models.User).filter(models.User.id == user_id)
+@router.post('/request-email-verification',
+             status_code=status.HTTP_202_ACCEPTED,
+             response_model=UserEmailOnly)
+def request_email_verification(user_email: UserEmailOnly,
+                               background_tasks: BackgroundTasks,
+                               db: Session = Depends(get_db)):
+    user_db = db.query(models.User).where(models.User.email == user_email.email).first()
 
-    return {'test': 'success'}
+    if not user_db:
+        raise HTTPException(detail=f'user with an email address of {user_email.email} was not found',
+                            status_code=status.HTTP_404_NOT_FOUND)
+
+    db_email_verification_request = db.query(models.EmailRequests) \
+        .where(models.EmailRequests.user_id == user_db.id
+               and models.EmailRequests.request_type == EmailRequestType.email_verification_request).first()
+
+    if db_email_verification_request:
+        cooldown_start = db_email_verification_request.created_at
+        cooldown_end = cooldown_start + timedelta(minutes=settings.MAIL_VERIFICATION_COOLDOWN_MINUTES)
+        now = datetime.now().astimezone()
+
+        if cooldown_end > now:
+            # noinspection PyTypeChecker
+            raise CooldownHTTPException(cooldown_end - now, detail=f'Too many '
+                                                                   f'verification requests, max 1 request per'
+                                                                   f' {settings.MAIL_VERIFICATION_COOLDOWN_MINUTES}'
+                                                                   f' minutes allowed')
+
+        db.delete(db_email_verification_request)
+
+    email_verification_request = create_email_request(user=user_db,
+                                                      token_type=TokenType.email_verification_token,
+                                                      request_type=EmailRequestType.email_verification_request)
+
+    db.add(email_verification_request)
+    db.commit()
+
+    content_language = db.query(models.Setting).where(models.Setting.name == AvailableSettings.language
+                                                      and models.Setting.user_id == user_db.id).first()
+
+    message, template_name = create_email_verification_email(content_language.current_value,
+                                                             user_db,
+                                                             email_verification_request.request_token)
+
+    background_tasks.add_task(send_email, message, template_name)
+
+    return user_email
+
+
+@router.put('/email-verification', response_model=ReturnUser)
+def verify_email(email_verification_request: EmailVerificationRequest, db: Session = Depends(get_db)):
+    request_db = db.query(models.EmailRequests) \
+        .where(models.EmailRequests.request_type == EmailRequestType.email_verification_request
+               and models.EmailRequests.request_token == email_verification_request.verification_token).first()
+
+    if not request_db:
+        raise HTTPException(detail='Invalid verification token provided',
+                            status_code=status.HTTP_401_UNAUTHORIZED)
+
+    token_db = request_db.request_token
+
+    if not token_db:
+        raise HTTPException(detail='Invalid verification token provided',
+                            status_code=status.HTTP_401_UNAUTHORIZED)
+
+    payload = oauth2.decode_jwt(email_verification_request.verification_token,
+                                expected_token_type=TokenType.email_verification_token,
+                                on_error=on_decode_error, db=db, request_db=request_db)
+
+    user = db.query(models.User).where(models.User.id == payload.user_id).first()
+
+    user.verified = True
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+@router.get('/me', response_model=ReturnUser, name='Get info about your account')
+async def me(db: Session = Depends(get_db),
+             user=Depends(oauth2.get_user)):
+    user_id = user.id
+    user = db.query(models.User).where(models.User.id == user_id).first()
+
+    return user
+
+
+@router.get('/users')
+def get_users():
+    pass
+
+
+@router.get('/user/{uuid}')
+def get_user_by_uuid():
+    pass
+
+
+@router.put('/ban/{uuid}')
+def block_user():
+    pass
+
+
+@router.put('/promote/{uuid}')
+def promote_user():
+    pass
+
+
+@router.put('/demote/{uuid}')
+def demote_user():
+    pass
