1f2d8cb84192b048b1713fda77733634abc6975b
Me-Phew <basmateusz@wp.pl>
2022-02-03 14:14:36 +0100

    Add passwords, service_events and permission_events tables

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP, ARRAY
 from sqlalchemy.dialects.postgresql import UUID
@@ -11,11 +12,21 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     name = Column(String, nullable=False)
     surname = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
     gender = Column(String, nullable=False)
     permission_level = Column(ARRAY(item_type=String), nullable=False, server_default='{user}')
     verified = Column(Boolean, nullable=False, server_default='false')
     disabled = Column(Boolean, nullable=False, server_default='false')
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+    settings = relationship('Setting')
+
+
+class Password(Base):
+    __tablename__ = 'passwords'
+    id = Column(Integer, primary_key=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    current = Column(Boolean, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
 
@@ -47,13 +58,38 @@ class Service(Base):
     name = Column(String, nullable=False, unique=True)
     min_price = Column(Integer, nullable=False)
     max_price = Column(Integer, nullable=False)
-    time_minutes = Column(Integer, nullable=False)
+    average_time_minutes = Column(Integer, nullable=False)
     description = Column(String)
     available = Column(Boolean, nullable=False)
-    user_defined = Column(Boolean, nullable=False)
-    created_by = Column(UUID(as_uuid=True), ForeignKey('users.id'))
+    deleted = Column(Boolean, nullable=False, server_default=text('false'))
+    created_by_user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    created_by = relationship("User")
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+
+
+class ServiceEvent(Base):
+    __tablename__ = 'service_events'
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, server_default=text('gen_random_uuid()'))
+    event_type = Column(String, nullable=False)
+    performed_by_user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    performed_on_service_id = Column(UUID(as_uuid=True), ForeignKey('services.id'), nullable=False)
+    performed_by = relationship('User', foreign_keys=[performed_by_user_id])
+    performed_on = relationship('Service', foreign_keys=[performed_on_service_id])
+    performed_at = Column(TIMESTAMP(timezone=True),
+                          nullable=False, server_default=text('now()'))
+
+
+class PermissionEvent(Base):
+    __tablename__ = 'permission_events'
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, server_default=text('gen_random_uuid()'))
+    event_type = Column(String, nullable=False)
+    performed_by_user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    performed_on_user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    performed_by = relationship('User', foreign_keys=[performed_by_user_id])
+    performed_on = relationship('User', foreign_keys=[performed_on_user_id])
+    performed_at = Column(TIMESTAMP(timezone=True),
+                          nullable=False, server_default=text('now()'))
 
 
 class Appointment(Base):
