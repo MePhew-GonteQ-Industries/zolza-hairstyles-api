c0e82ce38c7d2a2d09b7459bc0125f2b1167183a
Me-Phew <basmateusz@wp.pl>
2022-02-04 22:30:16 +0100

    Implement get services functionality and add scaffolds for other endpoints related to services

--- src/routers/services.py
+++ src/routers/services.py
@@ -1,13 +1,84 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, Depends
+from pydantic import UUID4
+from sqlalchemy.orm import Session
+
+from .. import models, oauth2
 from ..config import settings
+from ..database import get_db
+from ..exceptions import ResourceNotFoundException
+from ..schemas.service import CreateServices, ReturnService, ReturnServiceDetailed, ReturnServices, \
+    ReturnServicesDetailed
 
 router = APIRouter(prefix=settings.BASE_URL + '/services',
                    tags=['Services'])
 
 
-@router.get('')
-async def get_services():
-    return {'service': {
-        'id': '',
-        'price': '5'
-    }}
+@router.get('', response_model=ReturnServices)
+def get_services(db: Session = Depends(get_db)):
+    services = db.query(models.Service).all()
+
+    return {'services': services}
+
+
+@router.get('/details', response_model=ReturnServicesDetailed)
+def get_services_details(db: Session = Depends(get_db),
+                         _=Depends(oauth2.get_admin)):
+    services_details = db.query(models.Service).all()
+
+    return {'services': services_details}
+
+
+@router.get('/details/{{uuid}}', response_model=ReturnServiceDetailed)
+def get_service_details(uuid: UUID4,
+                        db: Session = Depends(get_db),
+                        _=Depends(oauth2.get_admin)):
+    service_details = db.query(models.Service).where(models.Service.id == uuid).first()
+
+    if not service_details:
+        raise ResourceNotFoundException()
+
+    return service_details
+
+
+@router.get('/{uuid}', response_model=ReturnService)
+def get_service(uuid: UUID4,
+                db: Session = Depends(get_db)):
+    service = db.query(models.Service).where(models.Service.id == uuid).first()
+
+    if not service:
+        raise ResourceNotFoundException()
+
+    return service
+
+
+@router.post('')
+def create_services(services: CreateServices,
+                    db: Session = Depends(get_db),
+                    admin=Depends(oauth2.get_admin)):
+    raise NotImplementedError
+
+    # return services
+
+
+@router.put('')
+def update_services(db: Session = Depends(get_db),
+                    _=Depends(oauth2.get_admin)):
+    raise NotImplementedError()
+
+
+@router.put('/service/{uuid}')
+def update_service(uuid: UUID4,
+                   db: Session = Depends(get_db),
+                   _=Depends(oauth2.get_admin)):
+    raise NotImplementedError()
+
+
+@router.delete('')
+def delete_services(services: List[UUID4],
+                    db: Session = Depends(get_db),
+                    admin=Depends(oauth2.get_admin)):
+    raise NotImplementedError
+    # print(services)
+    # return services
---
+++ src/schemas/service.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from pydantic import BaseModel, Field, UUID4, validator
+from .user import ReturnUserDetailed
+from typing import List, Optional, Union
+
+
+class Service(BaseModel):
+    name: str
+    min_price: int = Field(gt=0)
+    max_price: int = Field(gt=0)
+    average_time_minutes: int
+    description: Union[None, str]
+    available: bool
+
+    @validator('max_price')
+    def validate(cls, v):
+        if v.get('min_price') > v.get('max_price'):
+            raise ValueError('ensure max_price is greater than the min_price')
+        return v
+
+    class Config:
+        orm_mode = True
+
+
+class CreateService(Service):
+    description: Optional[str] = None
+
+
+class CreateServices(BaseModel):
+    services: List[Service]
+
+
+class ReturnService(Service):
+    id: UUID4
+    created_at: datetime
+
+
+class ReturnServiceDetailed(ReturnService):
+    created_by: ReturnUserDetailed
+
+
+class ReturnServices(BaseModel):
+    services: List[ReturnService]
+
+
+class ReturnServicesDetailed(BaseModel):
+    services: List[ReturnServiceDetailed]
