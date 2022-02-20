85c7c58e9da935cbdff33c57965086561399bddc
Me-Phew <basmateusz@wp.pl>
2022-02-20 22:48:49 +0100

    Move test database related code to seperate file

---
+++ tests/conf_database.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.database import Base, SQLALCHEMY_DATABASE_URL, get_db_engine, \
+    get_session
+
+SQLALCHEMY_TEST_DATABASE_URL = f'{SQLALCHEMY_DATABASE_URL}_test'
+
+
+database_engine = get_db_engine(SQLALCHEMY_TEST_DATABASE_URL)
+TestingSessionLocal = get_session(database_engine)
+
+
+@pytest.fixture
+def session():
+    Base.metadata.drop_all(bind=database_engine)
+    Base.metadata.create_all(bind=database_engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
