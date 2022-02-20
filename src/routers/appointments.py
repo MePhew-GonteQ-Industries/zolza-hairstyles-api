818c28a314669f6e4c59575cc8d0500d240453b3
Me-Phew <basmateusz@wp.pl>
2022-02-20 22:46:06 +0100

    Add scaffold version of this route

--- src/routers/appointments.py
+++ src/routers/appointments.py
@@ -1,32 +1,63 @@
-from fastapi import APIRouter, status
+import uuid
+from datetime import datetime, timedelta
+
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from .. import models, oauth2
 from ..config import settings
+from ..database import get_db
+from ..jobs import send_appointment_remainder
+from ..scheduler import scheduler
 
 router = APIRouter(prefix=settings.BASE_URL + "/appointments", tags=["Appointments"])
 
 
-@router.get("")
-async def get_appointments():
-    return {
-        "appointment": {
-            "id": 0,
-            "type": "",
-            "time": "15-1512515-12512512-251",
-            "created_at": "124-4124-4241",
-            "user_id": 5,
-        },
-    }
+@router.get('/mine')
+def get_your_appointments(db: Session = Depends(get_db),
+                          user=Depends(oauth2.get_user)):
+    appointments_db = db.query(models.Appointment).where(models.Appointment.user_id == user.id).all()
+
+    return {'appointments': appointments_db}
 
 
-@router.post("", status_code=status.HTTP_201_CREATED)
-async def create_appointment():
-    return {"": ""}
+@router.get('/mine/{id}')
+def get_your_appointment(db: Session = Depends(get_db),
+                         user=Depends(oauth2.get_verified_user)):
+    return NotImplementedError
 
 
-@router.get("/{id}", status_code=status.HTTP_201_CREATED)
-async def get_appointment():
-    pass
+@router.put('/mine/{id}', status_code=status.HTTP_201_CREATED)
+async def update_your_appointment(db: Session = Depends(get_db),
+                                  user=Depends(oauth2.get_verified_user)):
+    raise NotImplementedError
 
 
-@router.post("/{id}", status_code=status.HTTP_201_CREATED)
-async def update_appointment():
-    pass
+@router.post('', status_code=status.HTTP_201_CREATED)
+def create_appointment(db: Session = Depends(get_db),
+                       user=Depends(oauth2.get_verified_user)):
+
+    scheduler.add_job(send_appointment_remainder, trigger='date',
+                      id=str(uuid.uuid4()),
+                      name=f'Appointment Remainder User {user.id}',
+                      misfire_grace_time=20,
+                      next_run_time=datetime.now() + timedelta(days=-2))
+    raise NotImplementedError
+
+
+@router.get('/all')
+def get_all_appointments(db: Session = Depends(get_db),
+                         user=Depends(oauth2.get_admin)):
+    raise NotImplementedError
+
+
+@router.get('/any/{id}')
+async def get_any_appointment(db: Session = Depends(get_db),
+                              user=Depends(oauth2.get_admin)):
+    raise NotImplementedError
+
+
+@router.put('/any/{id}')
+def update_any_appointment(db: Session = Depends(get_db),
+                           user=Depends(oauth2.get_admin)):
+    raise NotImplementedError
