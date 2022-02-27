2f3251ae3207563511154f05ce51d07794d928ef
Me-Phew <basmateusz@wp.pl>
2022-02-27 19:34:52 +0100

    Improve code style and update api to reflect changes in oauth2 file

--- src/routers/appointments.py
+++ src/routers/appointments.py
@@ -14,7 +14,10 @@ router = APIRouter(prefix=settings.BASE_URL + "/appointments", tags=["Appointmen
 
 
 @router.get("/mine")
-def get_your_appointments(db: Session = Depends(get_db), user=Depends(oauth2.get_user)):
+def get_your_appointments(db: Session = Depends(get_db),
+                          user_session=Depends(oauth2.get_user)):
+    user = user_session.user
+
     appointments_db = (
         db.query(models.Appointment).where(models.Appointment.user_id == user.id).all()
     )
@@ -24,28 +27,32 @@ def get_your_appointments(db: Session = Depends(get_db), user=Depends(oauth2.get
 
 @router.get("/mine/{id}")
 def get_your_appointment(
-    db: Session = Depends(get_db), user=Depends(oauth2.get_verified_user)
+    db: Session = Depends(get_db),
+        verified_user_session=Depends(oauth2.get_verified_user)
 ):
     return NotImplementedError
 
 
 @router.put("/mine/{id}", status_code=status.HTTP_201_CREATED)
 async def update_your_appointment(
-    db: Session = Depends(get_db), user=Depends(oauth2.get_verified_user)
+    db: Session = Depends(get_db),
+        verified_user_session=Depends(oauth2.get_verified_user)
 ):
     raise NotImplementedError
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
 def create_appointment(
-    db: Session = Depends(get_db), user=Depends(oauth2.get_verified_user)
+    db: Session = Depends(get_db),
+        verified_user_session=Depends(oauth2.get_verified_user)
 ):
+    verified_user = verified_user_session.user
 
     scheduler.add_job(
         send_appointment_remainder,
         trigger="date",
         id=str(uuid.uuid4()),
-        name=f"Appointment Remainder User {user.id}",
+        name=f"Appointment Remainder User {verified_user.id}",
         misfire_grace_time=20,
         next_run_time=datetime.now() + timedelta(days=-2),
     )
@@ -53,19 +60,22 @@ def create_appointment(
 
 
 @router.get("/all")
-def get_all_appointments(db: Session = Depends(get_db), user=Depends(oauth2.get_admin)):
+def get_all_appointments(db: Session = Depends(get_db),
+                         admin_session=Depends(oauth2.get_admin)):
     raise NotImplementedError
 
 
 @router.get("/any/{id}")
 async def get_any_appointment(
-    db: Session = Depends(get_db), user=Depends(oauth2.get_admin)
+    db: Session = Depends(get_db),
+        admin_session=Depends(oauth2.get_admin)
 ):
     raise NotImplementedError
 
 
 @router.put("/any/{id}")
 def update_any_appointment(
-    db: Session = Depends(get_db), user=Depends(oauth2.get_admin)
+    db: Session = Depends(get_db),
+        admin_session=Depends(oauth2.get_admin)
 ):
     raise NotImplementedError
