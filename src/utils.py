74d607cf0c6eb65a6c1f0de93e2ae52e3fde4d17
Me-Phew <basmateusz@wp.pl>
2022-02-27 19:39:23 +0100

    Add verify_password function

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,77 @@ from src import models
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+def verify_password(*,
+                    password,
+                    user_id,
+                    db):
+    current_password_hash = (
+        db.query(models.Password.password_hash)
+        .where(models.Password.user_id == user_id)
+        .where(models.Password.current == True)
+        .first()
+    )
+
+    if not compare_passwords(password, *current_password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="invalid credentials"
+        )
+
+
+def change_password(*, new_password,
+                    user_id,
+                    db: Session) -> None:
+    recent_passwords = (
+        db.query(models.Password)
+        .where(models.Password.user_id == user_id)
+        .all()
+    )
+
+    for recent_password in recent_passwords:
+        if compare_passwords(
+            new_password, recent_password.password_hash
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="new password cannot be the same as any of the last 5 passwords",
+            )
+
+    old_passwords = (
+        db.query(models.Password)
+        .where(models.Password.user_id == user_id)
+        .where(models.Password.current == False)
+        .order_by(models.Password.created_at.desc())
+        .offset(4)
+        .all()
+    )
+
+    for old_password in old_passwords:
+        db.delete(old_password)
+
+    db.commit()
+
+    current_password = (
+        db.query(models.Password)
+        .where(models.Password.user_id == user_id)
+        .where(models.Password.current)
+        .first()
+    )
+
+    current_password.current = False
+
+    db.commit()
+
+    new_password = models.Password(
+        password_hash=hash_password(new_password),
+        user_id=user_id,
+        current=True,
+    )
+
+    db.add(new_password)
+
+    db.commit()
+
+
 def hash_password(password):
     return pwd_context.hash(password)
 
