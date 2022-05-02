44137f8e3b63eca645dd5bbad9457a3e3152b96a
Me-Phew <basmateusz@wp.pl>
2022-05-02 20:12:40 +0200

    Rebuild services with time slots system

--- src/routers/services.py
+++ src/routers/services.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+import langcodes
+from fastapi import APIRouter, Depends, Header
 from pydantic import UUID4
 from sqlalchemy.orm import Session
 
@@ -9,18 +10,39 @@ from ..config import settings
 from ..database import get_db
 from ..exceptions import ResourceNotFoundException
 from ..schemas.service import CreateServices, ReturnService, ReturnServiceDetailed
+from ..schemas.user_settings import DefaultContentLanguages
 
 router = APIRouter(prefix=settings.BASE_URL + "/services", tags=["Services"])
 
 
-@router.get("", response_model=List[ReturnService])
-def get_services(db: Session = Depends(get_db)):
-    services = db.query(models.Service).all()
+@router.get("", response_model=list[ReturnService])
+def get_services(db: Session = Depends(get_db),
+                 accept_language: str | None = Header(None)):
+    if accept_language:
+        language = langcodes.Language.get(
+            langcodes.standardize_tag(accept_language))
+
+        if language.is_valid():
+            language_code = language.language
+        else:
+            language_code = DefaultContentLanguages.polish.value
+    else:
+        language_code = DefaultContentLanguages.polish.value
+
+    services_db = db.query(models.Service, models.ServiceTranslations).join(
+        models.ServiceTranslations
+    ).join(models.Language).where(models.Language.code == language_code).all()
+
+    services = []
+    for service in services_db:
+        service[0].name = service[1].name
+        service[0].description = service[1].description
+        services.append(service[0])
 
     return services
 
 
-@router.get("/details", response_model=List[ReturnServiceDetailed])
+@router.get("/details", response_model=list[ReturnServiceDetailed])
 def get_services_details(db: Session = Depends(get_db), _=Depends(oauth2.get_admin)):
     services_details = db.query(models.Service).all()
 
@@ -29,7 +51,7 @@ def get_services_details(db: Session = Depends(get_db), _=Depends(oauth2.get_adm
 
 @router.get("/details/{uuid}", response_model=ReturnServiceDetailed)
 def get_service_details(
-    uuid: UUID4, db: Session = Depends(get_db), _=Depends(oauth2.get_admin)
+        uuid: UUID4, db: Session = Depends(get_db), _=Depends(oauth2.get_admin)
 ):
     service_details = db.query(models.Service).where(models.Service.id == uuid).first()
 
@@ -51,9 +73,9 @@ def get_service(uuid: UUID4, db: Session = Depends(get_db)):
 
 @router.post("")
 def create_services(
-    services: CreateServices,
-    db: Session = Depends(get_db),
-    admin_session=Depends(oauth2.get_admin),
+        services: CreateServices,
+        db: Session = Depends(get_db),
+        admin_session=Depends(oauth2.get_admin),
 ):
     raise NotImplementedError
 
@@ -67,16 +89,16 @@ def update_services(db: Session = Depends(get_db), _=Depends(oauth2.get_admin)):
 
 @router.put("/service/{uuid}")
 def update_service(
-    uuid: UUID4, db: Session = Depends(get_db), _=Depends(oauth2.get_admin)
+        uuid: UUID4, db: Session = Depends(get_db), _=Depends(oauth2.get_admin)
 ):
     raise NotImplementedError()
 
 
 @router.delete("")
 def delete_services(
-    services: List[UUID4],
-    db: Session = Depends(get_db),
-    admin_session=Depends(oauth2.get_admin),
+        services: List[UUID4],
+        db: Session = Depends(get_db),
+        admin_session=Depends(oauth2.get_admin),
 ):
     raise NotImplementedError
     # print(services)
