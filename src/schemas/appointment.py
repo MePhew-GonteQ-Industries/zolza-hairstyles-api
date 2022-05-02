13cc99655cce6ceba6ac0a9d8f9c4c4e770bcc7c
Me-Phew <basmateusz@wp.pl>
2022-05-02 20:13:20 +0200

    Add new schemas

--- src/schemas/appointment.py
+++ src/schemas/appointment.py
@@ -1,4 +1,39 @@
-from enum import Enum
-from pydantic import BaseModel, EmailStr, Field, validator, UUID4
-from .user_settings import ReturnSetting
-from typing import List
+import datetime
+
+from pydantic import BaseModel, UUID4
+
+
+class AppointmentSlot(BaseModel):
+    id: UUID4
+    occupied: bool
+    reserved: bool
+    reserved_reason: str | None
+    holiday: bool
+    sunday: bool
+    break_time: bool
+    date: datetime.date
+    start_time: datetime.datetime | None
+    end_time: datetime.datetime | None
+    holiday_name: str | None
+
+    class Config:
+        orm_mode = True
+
+
+class AppointmentSlotDetailed(AppointmentSlot):
+    occupied_by_appointment: None | UUID4
+
+
+class BaseAppointment(BaseModel):
+    service_id: UUID4
+
+    class Config:
+        orm_mode = True
+
+
+class CreateAppointment(BaseAppointment):
+    first_slot_id: UUID4
+
+
+class ReturnAppointment(BaseAppointment):
+    pass
