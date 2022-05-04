2575e20659c6f0e5c6abb3954c1a0fea3b61b440
Me-Phew <basmateusz@wp.pl>
2022-05-04 22:04:51 +0200

    Add custom logging config

--- src/utils.py
+++ src/utils.py
@@ -21,14 +21,22 @@ from .schemas.user_settings import AvailableSettings, DefaultContentLanguages
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s",
+                              "%Y-%m-%d %H:%M:%S")
+file_handler = logging.FileHandler(__file__)
+file_handler.setFormatter(formatter)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+
 
 def init_languages(db: Session) -> None:
     english = langcodes.Language.get(standardize_tag("en-GB"))
 
     english_db = (
         db.query(models.Language)
-        .where(models.Language.code == english.language)
-        .first()
+            .where(models.Language.code == english.language)
+            .first()
     )
 
     if not english_db:
@@ -63,8 +71,8 @@ def init_services(db: Session) -> None:
         for lang, name in names.items():
             service_translation = (
                 db.query(models.ServiceTranslations)
-                .where(models.ServiceTranslations.name == name)
-                .first()
+                    .where(models.ServiceTranslations.name == name)
+                    .first()
             )
 
             if service_translation:
@@ -76,8 +84,8 @@ def init_services(db: Session) -> None:
                 max_price=service["max_price"],
                 average_time_minutes=service["average_time_minutes"],
                 required_slots=(
-                    int(service["average_time_minutes"])
-                    // settings.APPOINTMENT_SLOT_TIME_MINUTES
+                        int(service["average_time_minutes"])
+                        // settings.APPOINTMENT_SLOT_TIME_MINUTES
                 ),
             )
             db.add(service_db)
@@ -87,8 +95,8 @@ def init_services(db: Session) -> None:
             for lang, name in names.items():
                 language_db = (
                     db.query(models.Language)
-                    .where(models.Language.code == lang)
-                    .first()
+                        .where(models.Language.code == lang)
+                        .first()
                 )
 
                 service_translation = models.ServiceTranslations(
@@ -112,9 +120,9 @@ def init_holidays(db: Session) -> None:
         for lang, name in holiday.items():
             holiday_db = (
                 db.query(models.Holiday)
-                .join(models.HolidayTranslations)
-                .where(models.HolidayTranslations.name == name)
-                .first()
+                    .join(models.HolidayTranslations)
+                    .where(models.HolidayTranslations.name == name)
+                    .first()
             )
 
             if holiday_db:
@@ -129,8 +137,8 @@ def init_holidays(db: Session) -> None:
             for lang, name in holiday.items():
                 language_db = (
                     db.query(models.Language)
-                    .where(models.Language.code == lang)
-                    .first()
+                        .where(models.Language.code == lang)
+                        .first()
                 )
 
                 holiday_translation = models.HolidayTranslations(
@@ -148,7 +156,7 @@ def ensure_enough_appointment_slots_available(get_db_func: callable) -> None:
 
 
 def ensure_appointment_slots_generation_task_exists(
-    background_scheduler: BackgroundScheduler,
+        background_scheduler: BackgroundScheduler,
 ) -> None:
     appointment_slots_generation_task = background_scheduler.get_job(
         "appointment_slots_generation"
@@ -159,7 +167,7 @@ def ensure_appointment_slots_generation_task_exists(
 
 
 def add_appointment_slots_generation_task(
-    background_scheduler: BackgroundScheduler,
+        background_scheduler: BackgroundScheduler,
 ) -> None:
     background_scheduler.add_job(
         ensure_enough_appointment_slots_available,
@@ -177,8 +185,8 @@ def add_appointment_slots_generation_task(
 def appointment_slots_generated(db: Session) -> bool:
     last_appointment_slot = (
         db.query(models.AppointmentSlot)
-        .order_by(models.AppointmentSlot.date.desc())
-        .first()
+            .order_by(models.AppointmentSlot.date.desc())
+            .first()
     )
 
     if not last_appointment_slot:
@@ -211,9 +219,9 @@ def generate_appointment_slots(db: Session) -> None:
     for holiday in holiday_names:
         holiday_id = (
             db.query(models.Holiday.id)
-            .join(models.HolidayTranslations)
-            .where(models.HolidayTranslations.name == list(holiday.values())[0])
-            .first()[0]
+                .join(models.HolidayTranslations)
+                .where(models.HolidayTranslations.name == list(holiday.values())[0])
+                .first()[0]
         )
         holiday_ids.append(holiday_id)
 
@@ -255,8 +263,8 @@ def generate_appointment_slots(db: Session) -> None:
 
     last_appointment_slot = (
         db.query(models.AppointmentSlot)
-        .order_by(models.AppointmentSlot.end_time.desc().nullslast())
-        .first()
+            .order_by(models.AppointmentSlot.end_time.desc().nullslast())
+            .first()
     )
 
     first_slot_start = None
@@ -349,8 +357,8 @@ def generate_appointment_slots(db: Session) -> None:
                 current_date = current_date + timedelta(days=1)
             else:
                 if (
-                    current_date.hour
-                    < weekplan[current_date.weekday()]["work_hours"]["start_hour"]
+                        current_date.hour
+                        < weekplan[current_date.weekday()]["work_hours"]["start_hour"]
                 ):
                     current_date = current_date.replace(
                         hour=weekplan[current_date.weekday()]["work_hours"][
@@ -362,8 +370,8 @@ def generate_appointment_slots(db: Session) -> None:
                     )
                     continue
                 elif (
-                    current_date.hour
-                    > weekplan[current_date.weekday()]["work_hours"]["end_hour"]
+                        current_date.hour
+                        > weekplan[current_date.weekday()]["work_hours"]["end_hour"]
                 ):
                     current_date = current_date + timedelta(days=1)
                     next_day_index = current_date.weekday() + 1
@@ -381,12 +389,13 @@ def generate_appointment_slots(db: Session) -> None:
                     current_date = current_date.replace(hour=hour, minute=minute)
                     continue
                 elif (
-                    current_date.hour
-                    == weekplan[current_date.weekday()]["work_hours"]["end_hour"]
+                        current_date.hour
+                        == weekplan[current_date.weekday()]["work_hours"]["end_hour"]
                 ):
                     if (
-                        current_date.minute
-                        >= weekplan[current_date.weekday()]["work_hours"]["end_minute"]
+                            current_date.minute
+                            >= weekplan[current_date.weekday()]["work_hours"][
+                        "end_minute"]
                     ):
                         current_date = current_date + timedelta(days=1)
                         next_day_index = current_date.weekday() + 1
@@ -411,7 +420,8 @@ def generate_appointment_slots(db: Session) -> None:
                                 date=current_date,
                                 start_time=current_date,
                                 end_time=current_date
-                                + timedelta(minutes=break_time["time_minutes"]),
+                                         + timedelta(
+                                    minutes=break_time["time_minutes"]),
                                 break_time=True,
                             )
                             current_date = current_date + timedelta(
@@ -423,8 +433,8 @@ def generate_appointment_slots(db: Session) -> None:
                 date=current_date,
                 start_time=current_date,
                 end_time=(
-                    current_date
-                    + timedelta(minutes=settings.APPOINTMENT_SLOT_TIME_MINUTES)
+                        current_date
+                        + timedelta(minutes=settings.APPOINTMENT_SLOT_TIME_MINUTES)
                 ),
             )
 
@@ -448,9 +458,9 @@ def start_scheduler() -> BackgroundScheduler:
 def get_user_language_id(db: Session, user_id: UUID4) -> int:
     language_code = (
         db.query(models.Setting.current_value)
-        .where(models.Setting.name == AvailableSettings.language.value)
-        .where(models.Setting.user_id == user_id)
-        .first()
+            .where(models.Setting.name == AvailableSettings.language.value)
+            .where(models.Setting.user_id == user_id)
+            .first()
     )
 
     if language_code:
@@ -458,8 +468,8 @@ def get_user_language_id(db: Session, user_id: UUID4) -> int:
 
     language_id = (
         db.query(models.Language.id)
-        .where(models.Language.code == language_code)
-        .first()
+            .where(models.Language.code == language_code)
+            .first()
     )
 
     if language_id:
@@ -468,8 +478,8 @@ def get_user_language_id(db: Session, user_id: UUID4) -> int:
     if not language_id:
         language_id = (
             db.query(models.Language.id)
-            .where(models.Language.code == DefaultContentLanguages.english)
-            .first()[0]
+                .where(models.Language.code == DefaultContentLanguages.english)
+                .first()[0]
         )
 
     return language_id
@@ -478,9 +488,9 @@ def get_user_language_id(db: Session, user_id: UUID4) -> int:
 def verify_password(*, password, user_id, db) -> None:
     current_password_hash = (
         db.query(models.Password.password_hash)
-        .where(models.Password.user_id == user_id)
-        .where(models.Password.current == True)
-        .first()
+            .where(models.Password.user_id == user_id)
+            .where(models.Password.current == True)
+            .first()
     )
 
     if not compare_passwords(password, *current_password_hash):
@@ -503,11 +513,11 @@ def change_password(*, new_password, user_id, db: Session) -> None:
 
     old_passwords = (
         db.query(models.Password)
-        .where(models.Password.user_id == user_id)
-        .where(models.Password.current == False)
-        .order_by(models.Password.created_at.desc())
-        .offset(4)
-        .all()
+            .where(models.Password.user_id == user_id)
+            .where(models.Password.current == False)
+            .order_by(models.Password.created_at.desc())
+            .offset(4)
+            .all()
     )
 
     for old_password in old_passwords:
@@ -517,9 +527,9 @@ def change_password(*, new_password, user_id, db: Session) -> None:
 
     current_password = (
         db.query(models.Password)
-        .where(models.Password.user_id == user_id)
-        .where(models.Password.current)
-        .first()
+            .where(models.Password.user_id == user_id)
+            .where(models.Password.current)
+            .first()
     )
 
     current_password.current = False
