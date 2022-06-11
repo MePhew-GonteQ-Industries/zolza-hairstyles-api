abdbd00f167f31b0ad06a95b02f054b2198c3fb2
Lint Action <lint-action@samuelmeuli.com>
2022-06-11 22:14:43 +0000

    Fix code style issues with Black

--- src/models.py
+++ src/models.py
@@ -235,10 +235,9 @@ class FcmToken(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     token = Column(String, nullable=False)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
-    session_id = Column(UUID(as_uuid=True),
-                        ForeignKey("sessions.id"),
-                        nullable=False,
-                        unique=True)
+    session_id = Column(
+        UUID(as_uuid=True), ForeignKey("sessions.id"), nullable=False, unique=True
+    )
     created_at = Column(
         TIMESTAMP(timezone=False), nullable=False, server_default=text("now()")
     )
--- src/routers/notifications.py
+++ src/routers/notifications.py
@@ -9,15 +9,19 @@ from ..schemas.notifications import FcmToken
 router = APIRouter(prefix=settings.BASE_URL + "/notifications", tags=["Notifications"])
 
 
-@router.post("/add_token", )  # response_model=
-def add_token(fcm_token: FcmToken,
-              db: Session = Depends(get_db),
-              user_session=Depends(oauth2.get_user)):
+@router.post(
+    "/add_token",
+)  # response_model=
+def add_token(
+    fcm_token: FcmToken,
+    db: Session = Depends(get_db),
+    user_session=Depends(oauth2.get_user),
+):
     user = user_session.user
     session = user_session.session
 
-    fcm_token_db = models.FcmToken(token=fcm_token.fcm_token,
-                                   user_id=user.id,
-                                   session_id=session.id)
+    fcm_token_db = models.FcmToken(
+        token=fcm_token.fcm_token, user_id=user.id, session_id=session.id
+    )
     db.add(fcm_token_db)
     db.commit()
