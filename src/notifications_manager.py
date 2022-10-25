5f7445a165761947b80cd5b51cce9a804caddaec
Me-Phew <basmateusz@wp.pl>
2022-10-25 22:10:11 +0200

    Create basic notifications manager structure

---
+++ src/notifications_manager.py
@@ -0,0 +1,10 @@
+class NotificationsManager:
+    def __int__(self):
+        pass
+
+    def get_notification_settings(self):
+        pass
+
+    def send_notification(self):
+        settings = self.get_notification_settings()
+
