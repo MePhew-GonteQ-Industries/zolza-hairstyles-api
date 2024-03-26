fb628857ce1256b63122bc74bd0ccff446895ef3
Me-Phew <basmateusz@wp.pl>
2024-03-26 21:20:03 +0100

    refactor: set default log_level to DEBUG for development

--- run_uvicorn.py
+++ run_uvicorn.py
@@ -4,4 +4,4 @@ from init_app import init_app
 
 if __name__ == "__main__":
     init_app()
-    uvicorn.run("src.main:app", log_level="info", reload=True)
+    uvicorn.run("src.main:app", log_level="debug", reload=True)
