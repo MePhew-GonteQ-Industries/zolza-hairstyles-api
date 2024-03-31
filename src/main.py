MePhew-GonteQ-Industries/zolza-hairstyles-api

commit 3d4f43c148765f37add77df31f19150b9533cdd8

refactor: disable github proxy when no configuration is provided

src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
@@ -52,6 +52,11 @@ def zolza_hairstyles_redirection():
 
 @app.get(settings.BASE_URL + "/github_user/{username}")
 def get_github_user_data(username: str):
+    if not settings.GH_APP_CLIENT_ID or settings.GH_APP_CLIENT_SECRET:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Path not found"
+        )
+
     data = github_client.get_user_data(username)
 
     return data

src/config.py
@@ -1,5 +1,5 @@
 from pydantic import EmailStr
-from pydantic_settings import SettingsConfigDict, BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -48,9 +48,9 @@ class Settings(BaseSettings):
     # FIREBASE config
     FIREBASE_SERVICE_ACCOUNT_CREDENTIALS_PATH: str
 
-    # GITHUB config
-    GH_APP_CLIENT_ID: str
-    GH_APP_CLIENT_SECRET: str
+    # GITHUB config (used for an unrelated proxy, disabled if not set)
+    GH_APP_CLIENT_ID: str | None = None
+    GH_APP_CLIENT_SECRET: str | None = None
     model_config = SettingsConfigDict(env_file=".env")
 
 

commit 70537e72d585846ba48c4ef6de3da985970d4c1c

fix: fix regression introduced in 3d4f43c

src/main.py
@@ -52,7 +52,7 @@ def frontend_redirection():
 
 @app.get(settings.BASE_URL + "/github_user/{username}")
 def get_github_user_data(username: str):
-    if not settings.GH_APP_CLIENT_ID or settings.GH_APP_CLIENT_SECRET:
+    if not settings.GH_APP_CLIENT_ID or not settings.GH_APP_CLIENT_SECRET:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Path not found"
         )