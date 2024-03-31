MePhew-GonteQ-Industries/zolza-hairstyles-api

commit 267a360cc8120b0ed6913b482488d782f32fb2ef

refactor: replace all specific names with COMPANY_NAME, polish email templates

src/github_client.py
@@ -1,12 +1,14 @@
-import requests
-from requests.auth import HTTPBasicAuth
-from .config import settings
 from functools import lru_cache
+
+import requests
 from fastapi import HTTPException, status
+from requests.auth import HTTPBasicAuth
+
+from .config import settings
 
 auth = HTTPBasicAuth(settings.GH_APP_CLIENT_ID, settings.GH_APP_CLIENT_SECRET)
 
-headers = {"Accept": "application/vnd.github+json", "User-Agent": "Zolza Hairstyles"}
+headers = {"Accept": "application/vnd.github+json", "User-Agent": settings.COMPANY_NAME}
 
 
 @lru_cache()

src/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     API_TITLE: str
     LOG_LEVEL: str = "INFO"
 
+    COMPANY_NAME: str
     BASE_URL: str
     FRONTEND_URL: str
 

src/email_manager.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 from pathlib import Path
 
@@ -48,10 +49,10 @@ def create_email_verification_email(
     match content_language:
         case DefaultContentLanguages.polish:
             template_name = "account_verification_pl.html"
-            subject = "Zołza Hairstyles - weryfikacja konta"
+            subject = f"{settings.COMPANY_NAME} - weryfikacja konta"
         case DefaultContentLanguages.english:
             template_name = "account_verification_en.html"
-            subject = "Zołza Hairstyles - account verification"
+            subject = f"{settings.COMPANY_NAME} - account verification"
         case _:
             raise InvalidEnumerationMemberHTTPException()
 
@@ -60,10 +61,12 @@ def create_email_verification_email(
         recipients=[user.email],
         template_body={
             "user": user.name,
-            "zolza_hairstyles_link": settings.FRONTEND_URL,
+            "frontend_url": settings.FRONTEND_URL,
             "account_confirmation_link": f"{settings.FRONTEND_URL}"
                                          f"/email-verification"
                                          f"?token={email_verification_token}",
+            "company_name": settings.COMPANY_NAME,
+            "current_year": datetime.datetime.today().year
         },
         subtype=MessageType.html,
     )
@@ -77,10 +80,10 @@ def create_password_reset_email(
     match content_language:
         case DefaultContentLanguages.polish:
             template_name = "password_reset_pl.html"
-            subject = "Zołza Hairstyles - resetowanie hasła"
+            subject = f"{settings.COMPANY_NAME} - resetowanie hasła"
         case DefaultContentLanguages.english:
             template_name = "password_reset_en.html"
-            subject = "Zołza Hairstyles - password reset"
+            subject = f"{settings.COMPANY_NAME} - password reset"
         case _:
             raise InvalidEnumerationMemberHTTPException()
 
@@ -91,6 +94,8 @@ def create_password_reset_email(
             "user": user.name,
             "password_reset_link": f"{settings.FRONTEND_URL}/password-reset"
                                    f"?token={password_reset_token}",
+            "company_name": settings.COMPANY_NAME,
+            "current_year": datetime.datetime.today().year
         },
         subtype=MessageType.html,
     )
@@ -131,7 +136,7 @@ def create_new_appointment_email(
     #         raise InvalidEnumerationMemberHTTPException()
 
     template_name = "new_appointment_pl.html"
-    subject = "Zołza Hairstyles - nowa wizyta"
+    subject = f"{settings.COMPANY_NAME} - nowa wizyta"
 
     message = MessageSchema(
         subject=subject,
@@ -139,6 +144,8 @@ def create_new_appointment_email(
         template_body={
             "title": title,
             "msg": msg,
+            "company_name": settings.COMPANY_NAME,
+            "current_year": datetime.datetime.today().year
         },
         subtype=MessageType.html,
     )

src/main.py
@@ -43,8 +43,8 @@ def startup():
     configure_and_start_scheduler()
 
 
-@app.get(settings.BASE_URL, tags=["Zołza Hairstyles Redirection"])
-def zolza_hairstyles_redirection():
+@app.get(settings.BASE_URL, tags=["Frontend Redirection"])
+def frontend_redirection():
     return RedirectResponse(
         settings.FRONTEND_URL, status_code=status.HTTP_308_PERMANENT_REDIRECT
     )

src/templates/account_verification_en.html
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Zołza Hairstyles - account activation</title>
+    <title>{{company_name}} - account activation</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 <body style="font-family: 'Open Sans', sans-serif;; text-align: center;">
-<h1>Hello, {{user}}!</h1> <!-- TODO: Polish all templates -->
-<h2>Thank you for signing up at <a href="{{zolza_hairstyles_link}}">{{zolza_hairstyles_link}}</a></h2>
+<h1>Hello, {{user}}!</h1>
+<h2>Thank you for signing up at <a href="{{frontend_url}}">{{frontend_url}}</a></h2>
 <h2 style="font-weight: normal;">In order to verify your account's email address click the below button</h2>
 <div style="text-align: center; display: flex; align-items: center; justify-content:center;">
     <a href="{{account_confirmation_link}}" style="display: block; text-decoration: none; margin: 5px 0;
@@ -23,6 +23,6 @@
           color: rgb(116, 120, 126); padding: 10px 20px; padding-left: 20px; padding-right: 20px; padding-top: 10px;
           padding-bottom: 10px; border-radius: 10px; font-size: 18px; text-align: left; word-break: break-all;">
     {{account_confirmation_link}}</p>
-<h2 style="font-weight: normal; text-align: center;">© 2021 Zołza Hairstyles</h2>
+<h2 style="font-weight: normal; text-align: center;">© {{current_year}} {{company_name}}</h2>
 </body>
 </html>

src/templates/account_verification_pl.html
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="pl">
-    <head>
-        <meta charset="UTF-8">
-        <title>Zołza Hairstyles - aktywacja konta</title>
-    </head>
-    <body style="font-family:arial,helvetica,sans-serif; text-align: center;">
-        <h1 style="font-size: 25px;">Witaj, {{user}}!</h1>
-        <h2>Dziękujemy za założenie konta na <a href="https://mephew.ddns.net">zolza-hairstyles.pl.</a></h2>
-        <h2 style="font-weight: normal;">Aby zweryfikować adres email swojego konta, kliknij poniższy przycisk</h2>
-        <div style="text-align: center; display: flex; align-items: center; justify-content:center;">
-            <a href="{{account_confirmation_link}}" style="display: block; text-decoration: none; margin: 5px 0;
+<head>
+    <meta charset="UTF-8">
+    <title>{{company_name}} - aktywacja konta</title>
+</head>
+<body style="font-family:arial,helvetica,sans-serif; text-align: center;">
+<h1 style="font-size: 25px;">Witaj, {{user}}!</h1>
+<h2>Dziękujemy za założenie konta na <a href="{{frontend_url}}">{{frontend_url}}.</a></h2>
+<h2 style="font-weight: normal;">Aby zweryfikować adres email swojego konta, kliknij poniższy przycisk</h2>
+<div style="text-align: center; display: flex; align-items: center; justify-content:center;">
+    <a href="{{account_confirmation_link}}" style="display: block; text-decoration: none; margin: 5px 0;
              background-color: rgb(34, 188, 102); color: white; padding: 10px 20px; padding-left: 20px; padding-right: 20px; padding-top:
-              10px; padding-bottom: 10px; font-size: 20px; border-radius: 10px; text-align: center; width: 50%;">Aktywuj konto</a>
-        </div>
-        <h2 style="font-weight: normal;">Jeżeli powyższy przycisk nie działa, skopiuj poniższy adres URL i wklej go w pasku wyszukiwania swojej przeglądarki</h2>
-        <p style="background-color: rgb(237, 239, 242); display: block; margin: 5px 0;
+              10px; padding-bottom: 10px; font-size: 20px; border-radius: 10px; text-align: center; width: 50%;">Aktywuj
+        konto</a>
+</div>
+<h2 style="font-weight: normal;">Jeżeli powyższy przycisk nie działa, skopiuj poniższy adres URL i wklej go w pasku
+    wyszukiwania swojej przeglądarki</h2>
+<p style="background-color: rgb(237, 239, 242); display: block; margin: 5px 0;
           color: rgb(116, 120, 126); padding: 10px 20px; padding-left: 20px; padding-right: 20px; padding-top: 10px;
-          padding-bottom: 10px; border-radius: 10px; font-size: 18px; text-align: left; word-break: break-all;">{{account_confirmation_link}}</p>
-        <h2 style="font-weight: normal; font-size: 15px; text-align: center;">© 2021 Zołza Hairstyles</h2>
-    </body>
+          padding-bottom: 10px; border-radius: 10px; font-size: 18px; text-align: left; word-break: break-all;">
+    {{account_confirmation_link}}</p>
+<h2 style="font-weight: normal; font-size: 15px; text-align: center;">© {{current_year}} {{company_name}}</h2>
+</body>
 </html>

src/templates/new_appointment_pl.html
@@ -2,10 +2,11 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Zołza Hairstyles - nowa wizyta</title>
+    <title>{{company_name}} - nowa wizyta</title>
 </head>
 <body style="font-family:arial,helvetica,sans-serif; text-align: center;">
 <h2>{{title}}</h2>
 <h2 style="font-weight: normal;">{{msg}}</h2>
+<h2 style="font-weight: normal; font-size: 15px; text-align: center;">© {{current_year}} {{company_name}}</h2>
 </body>
 </html>

src/templates/password_reset_en.html
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Zołza Hairstyles - password reset</title>
-    </head>
-    <body style="font-family:arial,helvetica,sans-serif; text-align: center;">
-        <h1 style="font-size: 25px;">Hello, {{user}}!</h1>
-        <h2>You are receiving this email because we received a password reset request for your account.</h2>
-        <h2 style="font-weight: normal;">In order to set your new password click the below button</h2>
-        <div style="text-align: center; display: flex; align-items: center; justify-content:center;"><a href="{{password_reset_link}}" style="display: block; text-decoration: none; margin: 5px 0; background-color: rgb(34, 188, 102); color: white; padding: 10px 20px; padding-left: 20px; padding-right: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 20px; border-radius: 10px; text-align: center; width: 50%;">Reset password</a></div>
-        <h2 style="font-weight: normal;">If the above button doesn't work, please copy the below URL and paste it into your web browser's search bar</h2>
-        <p style="background-color: rgb(237, 239, 242); display: block; margin: 5px 0;
+<head>
+    <meta charset="UTF-8">
+    <title>{{company_name}} - password reset</title>
+</head>
+<body style="font-family:arial,helvetica,sans-serif; text-align: center;">
+<h1 style="font-size: 25px;">Hello, {{user}}!</h1>
+<h2>You are receiving this email because we received a password reset request for your account.</h2>
+<h2 style="font-weight: normal;">In order to set your new password click the below button</h2>
+<div style="text-align: center; display: flex; align-items: center; justify-content:center;"><a
+        href="{{password_reset_link}}"
+        style="display: block; text-decoration: none; margin: 5px 0; background-color: rgb(34, 188, 102); color: white; padding: 10px 20px; padding-left: 20px; padding-right: 20px; padding-top: 10px; padding-bottom: 10px; font-size: 20px; border-radius: 10px; text-align: center; width: 50%;">Reset
+    password</a></div>
+<h2 style="font-weight: normal;">If the above button doesn't work, please copy the below URL and paste it into your web
+    browser's search bar</h2>
+<p style="background-color: rgb(237, 239, 242); display: block; margin: 5px 0;
           color: rgb(116, 120, 126); padding: 10px 20px; padding-left: 20px; padding-right: 20px; padding-top: 10px;
-          padding-bottom: 10px; border-radius: 10px; font-size: 18px; text-align: left; word-break: break-all;">{{password_reset_link}}</p>
-        <h2 style="font-weight: normal; text-align: left;">If you did not request a password reset, likely another user entered your email address by mistake while trying to reset their password.  If that's the case, you don't need to take any further action and can safely disregard this email.</h2>
-        <h2 style="font-weight: normal; font-size: 15px; text-align: center;">© 2021 Zołza Hairstyles</h2>
-    </body>
+          padding-bottom: 10px; border-radius: 10px; font-size: 18px; text-align: left; word-break: break-all;">
+    {{password_reset_link}}</p>
+<h2 style="font-weight: normal; text-align: left;">If you did not request a password reset, likely another user entered
+    your email address by mistake while trying to reset their password. If that's the case, you don't need to take any
+    further action and can safely disregard this email.</h2>
+<h2 style="font-weight: normal; font-size: 15px; text-align: center;">© {{current_year}} {{company_name}}</h2>
+</body>
 </html>

commit 7b3c2bdb140d98a8bcad5943f09f8fed92af4efb

fix: fix regression introduced in 267a360
fix UnicodeEncodeError that occurred when company name included characters incompatible with latin-1

src/github_client.py
@@ -8,7 +8,7 @@ from .config import settings
 
 auth = HTTPBasicAuth(settings.GH_APP_CLIENT_ID, settings.GH_APP_CLIENT_SECRET)
 
-headers = {"Accept": "application/vnd.github+json", "User-Agent": settings.COMPANY_NAME}
+headers = {"Accept": "application/vnd.github+json", "User-Agent": settings.COMPANY_NAME.encode('utf-8')}
 
 
 @lru_cache()